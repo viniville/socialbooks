e4c44c41545342800307347f68a0cfc39a58b401
Vinicius Ville <vinicius.ville@deltainf.com.br>
2018-01-10 17:08:16 -0200

    Initial commit

---
+++ src/main/java/com/algaworks/socialbooks/domain/Livro.java
@@ -0,0 +1,111 @@
+package com.algaworks.socialbooks.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+@Entity
+public class Livro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String autor;
+	private Date publicacao;
+	private String editora;
+	private String resumo;
+	
+	@OneToMany(mappedBy = "livro")
+	private List<Comentario> comentarios;
+	
+	public Livro() {
+		
+	}
+	
+	public Livro(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Date getPublicacao() {
+		return publicacao;
+	}
+	public void setPublicacao(Date publicacao) {
+		this.publicacao = publicacao;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public String getResumo() {
+		return resumo;
+	}
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.algaworks.socialbooks.handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.algaworks.socialbooks.domain.DetalhesErro;
+import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(LivroNaoEncontradoException.class)
+	public ResponseEntity<DetalhesErro> handleLivroNaoEncontratoException(LivroNaoEncontradoException e, HttpServletRequest request) {
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(404L);
+		erro.setTitulo("Livro não pôde ser encontrado");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
+		erro.setTimestamp(System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+
+}
